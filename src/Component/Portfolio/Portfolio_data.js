773791b266e4f6c2315d049617d7d1cd076e1597
Mahsa <mahsa@Mahsas-Laptop.local>
2023-06-30 16:42:39 +1000

    description

--- src/Component/Portfolio/Portfolio_data.js
+++ src/Component/Portfolio/Portfolio_data.js
@@ -6,6 +6,7 @@ const Portfolio_data = [
     title: "An application for Walking and Cycling ",
     image: "./image/Mapty.png",
     address: "https://m98hs98.github.io/mapty/",
+    description: "This application is a JavaScript program for a fitness tracking application called Mapty. It defines classes for workouts, specifically for running and cycling, with properties and methods to calculate pace, speed, and workout descriptions. The program utilizes a map library (Leaflet) to display the map, capture user input through a form, and render workout markers. It also implements local storage to persist workout data.",
   },
   {
     id: 2,
@@ -14,6 +15,7 @@ const Portfolio_data = [
     title: "An app for bank accounting",
     image: "./image/Bankist.png",
     address: "https://m98hs98.github.io/Bankist/",
+    description: "The provided code is a Bank Application with features for user banking operations. It handles logging in, money transfers, loan requests, account closure, and movement sorting. The JavaScript code manages user interactions, data manipulation, and UI updates. It includes functions for formatting, displaying movements, calculating balances, and creating usernames. The code represents a basic banking system with account management and transaction capabilities."
   },
   {
     id: 3,
@@ -22,6 +24,7 @@ const Portfolio_data = [
     title: "An application for ordernig healthy food",
     image: "./image/Omnifood.png",
     address: "https://m98hs98.github.io/Omnifood/",
+    description: "The code enhances a nutrition application using JavaScript. It adds a sticky navigation bar, smooth scrolling animations, and animations for visible elements. It also includes a mobile navigation feature. These interactive features improve the usability and engagement of the application.",
   },
   {
     id: 4,
@@ -30,6 +33,7 @@ const Portfolio_data = [
     title: "Playing game",
     image: "./image/pigGame.png",
     address: "https://m98hs98.github.io/Pig-Game/",
+    description: "The code implements a Pig game using JavaScript. It handles initializing the game, rolling the dice, updating scores, switching players, holding scores, and resetting the game. The game involves rolling a dice, accumulating scores, and reaching a winning score of 20. It provides the necessary functionality for playing and managing the game.",
   },
   {
     id: 5,
@@ -38,6 +42,7 @@ const Portfolio_data = [
     title: "Playing game",
     image: "./image/searchImage.png",
     address: "https://remarkable-gingersnap-29121b.netlify.app",
+    description: "The provided code is a React application for searching images. It consists of several components: 'App', 'ImageCard', 'ImageList', and 'SearchBar'. The 'App' component handles the state and API integration, while the 'ImageCard' component displays individual images with their descriptions. The 'ImageList' component maps the image results to 'ImageCard' components. The 'SearchBar' component accepts user input for image search terms. The 'index.js' file renders the 'App' component to the HTML element with the id of 'root', and the 'unsplash.js' file provides the necessary configuration for making requests to the Unsplash API. Overall, this code creates an image search application using React.",
   },
   {
     id: 6,
@@ -46,6 +51,7 @@ const Portfolio_data = [
     title: "Final project in Generation",
     image: "./image/Form.png",
     address: "https://ank1111-dev.github.io/Form/",
+    description: "The provided code consists of 'App.js' and 'TaskManager.js'. 'App.js' handles form input, validation, and task addition. 'TaskManager.js' manages tasks, including adding, retrieving, rendering, and deleting tasks. The code creates a form, validates input, and displays tasks based on their status.",
   },
 ];
 
